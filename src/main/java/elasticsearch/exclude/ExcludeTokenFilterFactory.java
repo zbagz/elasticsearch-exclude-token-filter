f13af76f69246d2481b87da7fc4168df533f72fa
zbagz <zbegsazj@pwrby.com>
2016-12-09 22:13:48 -0300

    Update ExcludeTokenFilterFactory.java

--- src/main/java/elasticsearch/exclude/ExcludeTokenFilterFactory.java
+++ src/main/java/elasticsearch/exclude/ExcludeTokenFilterFactory.java
@@ -1,37 +1,22 @@
 package org.elasticsearch.index.analysis.exclude;
 
-import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
-import org.elasticsearch.index.analysis.Analysis;
-
-import java.io.IOException;
-
 import org.apache.lucene.analysis.TokenStream;
-
+import org.apache.lucene.analysis.miscellaneous.UniqueTokenFilter;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.inject.assistedinject.Assisted;
-
-import org.elasticsearch.index.Index;
-import org.elasticsearch.index.settings.IndexSettings;
-
-import org.apache.lucene.util.Version;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.IndexSettings;
 
 public class ExcludeTokenFilterFactory extends AbstractTokenFilterFactory {
 
-    private String tokenSeparator = null;
-    private int incrementGap = 100;
-    
-    @Inject 
-    public ExcludeTokenFilterFactory(Index index, @IndexSettings Settings indexSettings, @Assisted String name, @Assisted Settings settings) {
-        super(index, indexSettings, name, settings);
-        // the token_separator is defined in the ES configuration file
-        tokenSeparator = settings.get("token_separator");
-	incrementGap = settings.getAsInt("increment_gap", 100);
+    private final boolean onlyOnSamePosition;
+
+    public ExcludeTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
+        super(indexSettings, name, settings);
+        this.onlyOnSamePosition = settings.getAsBoolean("only_on_same_position", false);
     }
 
-    @Override 
+    @Override
     public TokenStream create(TokenStream tokenStream) {
-        return new ExcludeFilter(Version.LATEST, tokenStream, tokenSeparator, incrementGap);
+        return new ExcludeTokenFilter(tokenStream, onlyOnSamePosition);
     }
-
 }
